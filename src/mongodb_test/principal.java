c7a83a1f2736de15ba92b77f9f10485c4392e01c
miltonmb <miltonmb@unitec.edu>
2017-11-18 20:32:39 -0600

    agregar empleado listo

--- src/mongodb_test/principal.java
+++ src/mongodb_test/principal.java
@@ -1,11 +1,16 @@
 package mongodb_test;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+import javax.swing.JOptionPane;
+import org.bson.Document;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author masterx
@@ -15,7 +20,13 @@ public class principal extends javax.swing.JFrame {
     /**
      * Creates new form principal
      */
+    MongoClientURI uri;
+    MongoClient client;
+    MongoDatabase db;
     public principal() {
+        uri = new MongoClientURI("mongodb://admin:admin@ds044709.mlab.com:44709/proyectoz");
+        client = new MongoClient(uri);
+        db = client.getDatabase(uri.getDatabase());
         initComponents();
     }
 
@@ -28,6 +39,24 @@ public class principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jd_Empleados = new javax.swing.JDialog();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jt_codAgregarEmpleados = new javax.swing.JTextField();
+        jt_pNombreAgregarEmpleado = new javax.swing.JTextField();
+        jt_sNombreAgregarEmpleado = new javax.swing.JTextField();
+        jt_pApellidoAgregarEmpleado = new javax.swing.JTextField();
+        jt_sApellidoAgregarEmpleado = new javax.swing.JTextField();
+        jt_cargoAgregarEmpleado = new javax.swing.JTextField();
+        bt_agregarEmpleado = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         bt_entrarPersonal = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
@@ -36,12 +65,148 @@ public class principal extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         bt_entrarZoologico = new javax.swing.JButton();
 
+        jLabel5.setText("Codigo del empleado:");
+
+        jLabel6.setText("Primer nombre:");
+
+        jLabel7.setText("Segundo nombre:");
+
+        jLabel8.setText("Primer apellido:");
+
+        jLabel9.setText("Segundo apellido:");
+
+        jLabel10.setText("Cargo:");
+
+        bt_agregarEmpleado.setText("Agregar");
+        bt_agregarEmpleado.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bt_agregarEmpleadoMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jt_codAgregarEmpleados, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jt_pNombreAgregarEmpleado))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jt_sNombreAgregarEmpleado))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jt_pApellidoAgregarEmpleado))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel9)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jt_sApellidoAgregarEmpleado))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel10)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jt_cargoAgregarEmpleado)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(bt_agregarEmpleado, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(bt_agregarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(jt_codAgregarEmpleados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(jt_pNombreAgregarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel7)
+                            .addComponent(jt_sNombreAgregarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel8)
+                            .addComponent(jt_pApellidoAgregarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel9)
+                            .addComponent(jt_sApellidoAgregarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel10)
+                            .addComponent(jt_cargoAgregarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(24, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Agregar Empleados", jPanel1);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 395, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 213, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Modificar Empleados", jPanel2);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 395, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 213, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Eliminar Empleados", jPanel3);
+
+        javax.swing.GroupLayout jd_EmpleadosLayout = new javax.swing.GroupLayout(jd_Empleados.getContentPane());
+        jd_Empleados.getContentPane().setLayout(jd_EmpleadosLayout);
+        jd_EmpleadosLayout.setHorizontalGroup(
+            jd_EmpleadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jTabbedPane1)
+        );
+        jd_EmpleadosLayout.setVerticalGroup(
+            jd_EmpleadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jTabbedPane1)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Sistema de Mantenimiento del Zoologico de Cordoba");
 
         bt_entrarPersonal.setText("Entrar");
+        bt_entrarPersonal.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bt_entrarPersonalMouseClicked(evt);
+            }
+        });
+        bt_entrarPersonal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt_entrarPersonalActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("Personal:");
 
@@ -99,6 +264,39 @@ public class principal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void bt_entrarPersonalMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_entrarPersonalMouseClicked
+
+    }//GEN-LAST:event_bt_entrarPersonalMouseClicked
+
+    private void bt_agregarEmpleadoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_agregarEmpleadoMouseClicked
+        String id, pNombre, sNombre, pApellido, sApellido, cargo;
+        try {
+            id = this.jt_codAgregarEmpleados.getText();
+            pNombre = this.jt_pNombreAgregarEmpleado.getText();
+            sNombre = this.jt_sNombreAgregarEmpleado.getText();
+            pApellido = this.jt_pApellidoAgregarEmpleado.getText();
+            sApellido = this.jt_sApellidoAgregarEmpleado.getText();
+            cargo = this.jt_cargoAgregarEmpleado.getText();
+            Document doc = new Document();
+            doc.append("CodigoEmpleado", id);
+            doc.append("PrimerNombre", pNombre);
+            doc.append("SegundoNombre", sNombre);
+            doc.append("PrimerApellido", pApellido);
+            doc.append("SegundoApellido", sApellido);
+            doc.append("Cargo", cargo);
+            db.getCollection("Empleados").insertOne(doc);
+            JOptionPane.showMessageDialog(this, "Se ha agregado");
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_bt_agregarEmpleadoMouseClicked
+
+    private void bt_entrarPersonalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_entrarPersonalActionPerformed
+        this.jd_Empleados.setResizable(false);
+        this.jd_Empleados.setLocationRelativeTo(this);
+        this.jd_Empleados.pack();
+        this.jd_Empleados.setVisible(true);
+    }//GEN-LAST:event_bt_entrarPersonalActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -135,12 +333,30 @@ public class principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bt_agregarEmpleado;
     private javax.swing.JButton bt_entrarAnimales;
     private javax.swing.JButton bt_entrarPersonal;
     private javax.swing.JButton bt_entrarZoologico;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JDialog jd_Empleados;
+    private javax.swing.JTextField jt_cargoAgregarEmpleado;
+    private javax.swing.JTextField jt_codAgregarEmpleados;
+    private javax.swing.JTextField jt_pApellidoAgregarEmpleado;
+    private javax.swing.JTextField jt_pNombreAgregarEmpleado;
+    private javax.swing.JTextField jt_sApellidoAgregarEmpleado;
+    private javax.swing.JTextField jt_sNombreAgregarEmpleado;
     // End of variables declaration//GEN-END:variables
 }
